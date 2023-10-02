2cf8d6a3ab1aa4c7f685db0501cd783ee654dfde
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-10-02 03:16:06 -0300

    Test 2: When user O win all cases test

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -25,3 +25,24 @@ TEST(VelhaTests, winnerX) {
     EXPECT_EQ(
         verifica_vencedor({{2, 2, 1}, {0, 2, 0}, {1, 1, 1}}), 1);  // Linha 3
 }
+
+TEST(VelhaTests, winnerO) {
+    EXPECT_EQ(
+        verifica_vencedor({{2, 1, 0}, {0, 2, 1}, {0, 1, 2}})
+        , 2);  // Diagonal Principal
+    EXPECT_EQ(
+        verifica_vencedor({{0, 1, 2}, {0, 2, 0}, {2, 1, 0}})
+        , 2);  // Diagonal Secundaria
+    EXPECT_EQ(
+        verifica_vencedor({{2, 1, 0}, {2, 1, 0}, {2, 0, 0}}), 2);  // Coluna 1
+    EXPECT_EQ(
+        verifica_vencedor({{0, 2, 1}, {0, 2, 0}, {1, 2, 0}}), 2);  // Coluna 2
+    EXPECT_EQ(
+        verifica_vencedor({{1, 0, 2}, {1, 1, 2}, {2, 1, 2}}), 2);  // Coluna 3
+    EXPECT_EQ(
+        verifica_vencedor({{2, 2, 2}, {1, 0, 0}, {0, 1, 0}}), 2);  // Linha 1
+    EXPECT_EQ(
+        verifica_vencedor({{1, 0, 0}, {2, 2, 2}, {0, 1, 0}}), 2);  // Linha 2
+    EXPECT_EQ(
+        verifica_vencedor({{1, 1, 2}, {0, 1, 0}, {2 ,2, 2}}), 2);  // Linha 3
+}
