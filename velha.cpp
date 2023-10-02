d6ebf4b4e6d5ee1714cef0e3c53d0df52ae754a3
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-10-02 00:44:07 -0300

    Fix: Fix the file with cpplint

--- velha.cpp
+++ velha.cpp
@@ -1,8 +1,10 @@
+// Copyright (c) 2023 Seu Alan Henrique Rangel Ferreira
+
 #include <iostream>
 #include <vector>
-#include "velha.hpp"  // Inclua o arquivo de cabeçalho
+#include "./velha.hpp"  // Inclua o arquivo de cabeçalho
 
-int main(){
-    vector<vector<int>> jogo = {{1,2,0},{0,2,1},{1,2,0}};
-    cout << verifica_vencedor(jogo)<<endl;
+int main() {
+    vector<vector<int>> jogo = {{1, 2, 0}, {0, 2, 1}, {1, 2, 0}};
+    cout << verifica_vencedor(jogo) << endl;
 }
--- velha.hpp
+++ velha.hpp
@@ -1,3 +1,5 @@
+// Copyright (c) 2023 Seu Alan Henrique Rangel Ferreira
+
 #ifndef velha_hpp
 #define velha_hpp
 #include <iostream>
